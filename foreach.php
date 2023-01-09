e9a3a170f8f25d4161df2e2c82807ec591f9b8c4
Esma Dinler <101185632+esmadinler@users.noreply.github.com>
2023-01-09 17:04:40 +0300

    günler

---
+++ foreach.php
@@ -0,0 +1,9 @@
+<?php
+$a=0;
+$gunler=array("pazartesi","salı","çarşamba","perşembe","cuma","cumartesi","pazar");
+foreach($gunler as $b)
+{
+	echo $a++." index değeri ".$b."<br>";
+}
+echo "<br>";echo "<br>";
+?>
